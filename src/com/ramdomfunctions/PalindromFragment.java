4dcb13bcf927bd3e1bbbdb0837133fd938b7681a
Xavier Bauquet <xavier.bauquet@gmail.com>
2016-04-07 09:49:43 +0200

    IMPLEMENT: save the input for palindrome

--- src/com/ramdomfunctions/PalindromFragment.java
+++ src/com/ramdomfunctions/PalindromFragment.java
@@ -1,6 +1,8 @@
 package com.ramdomfunctions;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -13,17 +15,22 @@ import android.widget.TextView;
 public class PalindromFragment extends Fragment {
 
 	View view;
-
+	SharedPreferences prefs;
+	EditText editText;
+	TextView textView;
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		view = inflater.inflate(R.layout.activity_palindrom_fragment, container, false);
 
-		EditText editText = (EditText) view.findViewById(R.id.input);
-
+		editText = (EditText) view.findViewById(R.id.input);
+		textView = (TextView) view.findViewById(R.id.output);
+		
+		prefs = getActivity().getPreferences(Context.MODE_PRIVATE);
+		
 		editText.addTextChangedListener(new TextWatcher() {
 			@Override
 			public void onTextChanged(CharSequence s, int start, int before, int count) {
-				TextView textView = (TextView) view.findViewById(R.id.output);
 				TextView isPalindromText = (TextView) view.findViewById(R.id.is_palindrom);
 				String invert = stringEnvers(s.toString());
 				textView.setText(invert);
@@ -42,6 +49,24 @@ public class PalindromFragment extends Fragment {
 		return view;
 	}
 
+	@Override
+	public void onPause() {
+		// Save the editText input 
+		SharedPreferences.Editor editor = prefs.edit();
+		editor.putString("palindrom", editText.getText().toString());
+		editor.commit();
+		super.onPause();
+	}
+	
+	@Override
+	public void onResume() {
+		// Restore the values of editText and textView
+		String string = prefs.getString("palindrom","");
+		editText.setText(string);
+		textView.setText(stringEnvers(string));
+		super.onResume();
+	}
+	
 	public String stringEnvers(String word) {
 		StringBuffer buffer = new StringBuffer();
 		for (int i = word.length() - 1; i >= 0; i--) {
